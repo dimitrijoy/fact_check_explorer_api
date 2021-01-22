d4c038e293188e2e1e207daa8c6a309e5d4fc30e
Dimitri Joy <36512319+dimitrijoy@users.noreply.github.com>
2021-01-21 21:48:26 -0600

    Handle missing element exceptions
    Smart search option also added to fetch() for improved querying.

--- fact_check_explorer_api.py
+++ fact_check_explorer_api.py
@@ -1,37 +1,55 @@
+# dependencies
 from constants import CLAIMANT_END, CLAIMANT_START, TIME_LIMIT
+from fuzzywuzzy import fuzz
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.ui import WebDriverWait
 
 # driver options
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 
 # gets top result of query, including claimant, claim, rating, and reference
-def fetch(query):
-    query = prepare(query)
+def fetch(query, smart=False):
     driver = webdriver.Chrome(options=options)
-    driver.get(f"https://toolbox.google.com/factcheck/explorer/search/{query};hl=en")
+    driver.get(f"https://toolbox.google.com/factcheck/explorer/search/{prepare(query)};hl=en")
     try:
-        claimant = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_element_located((By.XPATH, ".//div[@title='Claimant']"))
-        )
-        claim = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_element_located((By.XPATH, ".//div[@title='Claim text']"))
-        )
-        rating = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_element_located((By.XPATH, ".//span[@title='Publisher rating']"))
-        )
-        source = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_element_located((By.XPATH, ".//span[@title='Publisher name']"))
-        )
-        reference = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_element_located((By.XPATH, ".//a[@title='View article in a new window']"))
-        )
-        return package(claimant.text[CLAIMANT_START:CLAIMANT_END], claim.text, rating.text, source.text, reference.get_attribute("href"))
+        if smart:
+            cards = WebDriverWait(driver, TIME_LIMIT).until(
+                EC.presence_of_all_elements_located((By.XPATH, ".//div[@class='fc-card-content']"))
+            )
+            fact_checks, index, max_set_ratio = [], 0, 0
+            for i in range(len(cards)):
+                card = cards[i]
+                try: # claimant is sometimes missing
+                    claimant = card.find_element_by_xpath(".//div[@title='Claimant']").text[CLAIMANT_START:CLAIMANT_END]
+                except NoSuchElementException:
+                    claimant = None
+                claim = card.find_element_by_xpath(".//div[@title='Claim text']").text
+                rating = card.find_element_by_xpath(".//span[@title='Publisher rating']").text
+                source = card.find_element_by_xpath(".//span[@title='Publisher name']").text
+                reference = card.find_element_by_xpath(".//a[@title='View article in a new window']").get_attribute("href")
+                fact_checks.append(package(claimant, claim, rating, source, reference))
+                set_ratio = fuzz.token_set_ratio(query, claim)
+                if set_ratio > max_set_ratio:
+                    index = i
+                    max_set_ratio = set_ratio
+            return fact_checks[index]
+        else:
+            card = WebDriverWait(driver, TIME_LIMIT).until(
+                EC.presence_of_element_located((By.XPATH, ".//div[@class='fc-card-content']"))
+            )
+            try: # claimant is sometimes missing
+                claimant = card.find_element_by_xpath(".//div[@title='Claimant']").text[CLAIMANT_START:CLAIMANT_END]
+            except NoSuchElementException:
+                claimant = None
+            claim = card.find_element_by_xpath(".//div[@title='Claim text']").text
+            rating = card.find_element_by_xpath(".//span[@title='Publisher rating']").text
+            source = card.find_element_by_xpath(".//span[@title='Publisher name']").text
+            reference = card.find_element_by_xpath(".//a[@title='View article in a new window']").get_attribute("href")
+            return package(claimant, claim, rating, source, reference)
     except TimeoutException:
         pass
     finally:
@@ -40,29 +58,24 @@ def fetch(query):
 
 # gets all results of query, including claimant, claim, rating, and reference for each
 def fetchall(query):
-    query = prepare(query)
     driver = webdriver.Chrome(options=options)
-    driver.get(f"https://toolbox.google.com/factcheck/explorer/search/{query};hl=en")
+    driver.get(f"https://toolbox.google.com/factcheck/explorer/search/{prepare(query)};hl=en")
     try:
-        claimants = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_all_elements_located((By.XPATH, ".//div[@title='Claimant']"))
+        cards = WebDriverWait(driver, TIME_LIMIT).until(
+            EC.presence_of_all_elements_located((By.XPATH, ".//div[@class='fc-card-content']"))
         )
-        claims = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_all_elements_located((By.XPATH, ".//div[@title='Claim text']"))
-        )
-        ratings = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_all_elements_located((By.XPATH, ".//span[@title='Publisher rating']"))
-        )
-        sources = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_all_elements_located((By.XPATH, ".//span[@title='Publisher name']"))
-        )
-        references = WebDriverWait(driver, TIME_LIMIT).until(
-            EC.presence_of_all_elements_located((By.XPATH, ".//a[@title='View article in a new window']"))
-        )
-        res = []
-        for i in range(len(claimants)):
-            res.append(package(claimants[i].text[CLAIMANT_START:CLAIMANT_END], claims[i].text, ratings[i].text, sources[i].text, references[i].get_attribute("href")))
-        return res
+        fact_checks = []
+        for card in cards:
+            try: # claimant is sometimes missing
+                claimant = card.find_element_by_xpath(".//div[@title='Claimant']").text[CLAIMANT_START:CLAIMANT_END]
+            except NoSuchElementException:
+                claimant = None
+            claim = card.find_element_by_xpath(".//div[@title='Claim text']").text
+            rating = card.find_element_by_xpath(".//span[@title='Publisher rating']").text
+            source = card.find_element_by_xpath(".//span[@title='Publisher name']").text
+            reference = card.find_element_by_xpath(".//a[@title='View article in a new window']").get_attribute("href")
+            fact_checks.append(package(claimant, claim, rating, source, reference))
+        return fact_checks
     except TimeoutException:
         pass
     finally:
